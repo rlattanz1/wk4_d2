ea0dcdf41918af6b0734c33dacb838e4b0a109d4
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-04-18 13:17:15 -0700

    inheritance practice assignment testing

--- practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
@@ -1,4 +1,20 @@
 class Employee
 
+    attr_reader :name, :title, :salary, :boss
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
+
+    def bonus(multiplier)
+        bonus = salary * multiplier
+    end
 
 end
+
+e1 = Employee.new('ned', 'founder', 1,000,000, 'nil')
+e2 = Employee.new('darren', 'TA manager', 78,000, 'ned')
+e3 = Employee.new('shawna', 'TA', 12,000, 'darren')
+e4 = Employee.new('david', 'TA', 10,000, 'darren')
--- practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
@@ -1,4 +1,19 @@
-class Manager
+require_relative 'employee.rb'
 
+class Manager < Employee
+
+    def initialize(name, title, salary, boss, underlings)
+        super(name, title, salary, boss)
+        @underlings = underlings
+    end
+
+    def bonus(multiplier)
+        bonus = (self.underlings.map {|underling| underling.salary}).sum * multiplier
+    end
 
 end
+
+m1 = Manager.new('ned', 'founder', 1,000,000, 'nil', [e2, e3, e4])
+m2 = Manager.new('darren', 'TA manager', 78,000, 'ned', [e3, e4])
+
+p m1.bonus
