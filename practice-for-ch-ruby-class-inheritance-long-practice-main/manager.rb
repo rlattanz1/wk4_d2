a294f576677f71cf845ffd6c3cfd9fee307f320d
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-04-18 13:31:08 -0700

    testing the inheritance

--- practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
@@ -7,13 +7,20 @@ class Manager < Employee
         @underlings = underlings
     end
 
+    # def underlings(employee)
+    #     if employee.boss == self.name
+    #         @underlings << employee
+    #     end
+    # end
+
     def bonus(multiplier)
         bonus = (self.underlings.map {|underling| underling.salary}).sum * multiplier
     end
 
 end
 
-m1 = Manager.new('ned', 'founder', 1,000,000, 'nil', [e2, e3, e4])
+
+m1 = Manager.new('ned', 'founder', 1,000,000, 'nil', ['darren', 'shawna', 'david'])
 m2 = Manager.new('darren', 'TA manager', 78,000, 'ned', [e3, e4])
 
-p m1.bonus
+p m1.bonus(5)
