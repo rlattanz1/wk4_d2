f9f7fc3fa5dbac508a7509e2e2a57bd58d0c2fc7
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-04-18 16:54:02 -0700

    merge changes

--- practice-for-ch-ruby-chess-long-practice-main/pieces/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/pieces/piece.rb
@@ -1,4 +1,4 @@
-require_relative '../board.rb'
+
 
 class Piece
 attr_reader :color, :board, :position
--- practice-for-ch-ruby-chess-long-practice-main/pieces/sliddable.rb
+++ practice-for-ch-ruby-chess-long-practice-main/pieces/sliddable.rb
@@ -2,5 +2,9 @@
 
 module Slideable
 
+    HORIZONTAL_DIRS = []
+    DIAGONAL_DIRS = []
+
+    Def initialize
 
 end
