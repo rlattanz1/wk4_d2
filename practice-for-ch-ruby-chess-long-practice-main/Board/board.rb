e191251175b12434f72e6b5180428b3bfe6bdd3a
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-04-18 16:04:42 -0700

    Co-authored-by: vincentpham123 <vincentpham123@users.noreply.github.com>

--- practice-for-ch-ruby-chess-long-practice-main/Board/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/Board/board.rb
@@ -1,2 +1,46 @@
-require_relative 'pieces'
+
+# require './pieces/piece.rb'
 class Board
+
+    attr_reader :rows, :null_piece
+    def initialize()
+        @rows = Array.new(8) {Array.new(8)}
+        @rows.each_with_index do |row,index|
+            row.each_with_index do |col, inx|
+                if index.between?(2,5)
+                    @rows[index][inx]  = '@null_piece'
+                else
+                    @rows[index][inx] = 'Piece.new'
+                end
+            end
+        end
+
+        # @null_piece = NullPiece.new
+    end
+
+    def [](pos)
+        r, c = pos
+        rows[r][c]
+    end
+
+    def []=(pos, piece)
+        r, c = pos
+        rows[r][c]= piece
+    end
+
+    def move_piece(color, start_pos, end_pos)
+        r, c = end_pos
+        if self[start_pos] != @null_piece && r.between?(0,7) && c.between?(0,7)
+            self[end_pos]= self[start_pos]
+            self[start_pos]= '@null_piece'
+        else
+            raise "invalid move"
+        end
+    end
+
+    def valid_pos(pos)
+
+    end
+
+
+end
